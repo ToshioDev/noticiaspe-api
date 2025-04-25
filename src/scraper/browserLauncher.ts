810b958410b6fbe3cc8e961c58672b2b022c8dc0
Alexis Leon Reyes <83489240+ToshioDev@users.noreply.github.com>
2025-04-25 13:51:13 -0600

    fix: browserLauncher production

--- src/scraper/browserLauncher.ts
+++ src/scraper/browserLauncher.ts
@@ -1,17 +1,26 @@
-import puppeteer, { Browser as Browser} from 'puppeteer';
-import puppeteerCore, { Browser as BrowserCore } from 'puppeteer-core';
+import puppeteer from 'puppeteer';
+import puppeteerCore from 'puppeteer-core';
 import chromium from '@sparticuz/chromium-min';
 
 /**
  * Devuelve una instancia de browser lista para producción o desarrollo.
  * Usa @sparticuz/chromium-min en producción (Vercel, serverless), y puppeteer local en desarrollo.
  */
-export async function launchBrowser(options: any = {}): Promise<Browser | BrowserCore> {
-  let browser: Browser | BrowserCore;
-  browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    ...options,
-  });
-  return browser;
+export async function launchBrowser(options: any = {}) {
+  if (process.env.NODE_ENV === 'production' || process.env.VERCEL_ENV === 'production') {
+    const executablePath = await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar');
+    return puppeteerCore.launch({
+      executablePath,
+      args: chromium.args,
+      headless: chromium.headless,
+      defaultViewport: chromium.defaultViewport,
+      ...options,
+    });
+  } else {
+    return puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      ...options,
+    });
+  }
 }
